4973cf35e07686b41e8c459b9fcfa2cfcbce0a73
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-05-21 03:32:44 -0300

    Refatorado firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
-import firebase from 'firebase';
+import { initializeApp } from 'firebase';
 
-const config = {
+const app = initializeApp({
   apiKey: 'AIzaSyC_F2fi2gcxY6kTSD_7FVQGVgNZOWUR0lw',
   authDomain: 'address-book-d2077.firebaseapp.com',
   databaseURL: 'https://address-book-d2077.firebaseio.com',
   projectId: 'address-book-d2077',
   storageBucket: 'address-book-d2077.appspot.com',
   messagingSenderId: '152794032932',
-};
+});
 
-const app = firebase.initializeApp(config);
-
-export default app;
+export const fireDB = app.database();
+export const addressRef = app.database().ref('address');
