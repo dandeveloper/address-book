94b2dea7744475df88c5fd8e41996b0f0ceaef1c
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-05-23 23:42:32 -0300

    Redesign Login and Register structure

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,34 +1,33 @@
 <template>
   <div class="container">
     <div class="row justify-content-md-center">
-      <form class="form col-md-6 col-lg-4 text-justify">
-        <p v-if="errors.length">
-          <ul>
-            <li class="alert alert-danger" v-for="error in errors" :key="error">{{ error }}</li>
-          </ul>
-        </p>
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input type="email" id="email" class="form-control"
-          placeholder="meu@email.com" v-model="form.email" required>
+      <div class="col-sm-6 col-md-4 col-md-offset-4">
+        <h1 class="text-center login-title">Faça seu Login</h1>
+        <div class="account-wall">
+          <img id="profile-img" class="profile-img-card"
+          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+          <p v-if="errors.length">
+            <ul class="alert alert-danger">
+              <li v-for="error in errors" :key="error">{{ error }}</li>
+            </ul>
+          </p>
+          <form class="form-signin">
+              <input type="email" id="email" class="form-control"
+              placeholder="meu@email.com" v-model="form.email" required>
+              <input type="password" id="password" class="form-control"
+              placeholder="Digite sua Senha..." v-model="form.password" required>
+              <button @click="login" class="btn btn-lg btn-primary btn-block">Entrar</button>
+            <label class="checkbox pull-left">
+              <input type="checkbox" id="rememberme"
+              v-model="form.remoberMe">
+              Lembrar-me
+            </label>
+          </form>
         </div>
-        <div class="form-group">
-          <label for="password">Senha</label>
-          <input type="password" id="password" class="form-control"
-          placeholder="Digite sua Senha..." v-model="form.password" required>
-        </div>
-        <div class="form-group">
-          <input type="checkbox" id="rememberme" class="form-check-input" v-model="form.remoberMe">
-          <label class="form-check-label" for="rememberme">Lembrar-me</label>
-        </div>
-        <div class="float-right">
-          <router-link :to="'/register'" class="btn btn-secondary">
-            Cadastrar
-          </router-link>
-          <!-- <button class="btn btn-secondary">Não sou cadastrado</button> -->
-          <button @click="login" class="btn btn-primary">Entrar</button>
-        </div>
-      </form>
+        <router-link :to="'/register'" class="text-center new-account">
+          Não possuo cadastro
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -71,7 +70,6 @@ export default {
         .signInWithEmailAndPassword(form.email, form.password)
         .then((user) => {
           this.doLogin(user);
-          alert('logado');
         })
         .catch((error) => {
           this.loginError(error);
@@ -83,6 +81,7 @@ export default {
         id: user.uid,
         auth: true,
       });
+      this.$router.push('/panel');
     },
 
     loginError(e) {
@@ -102,4 +101,82 @@ export default {
 </script>
 
 <style scoped>
+  .form-signin {
+    max-width: 330px;
+    padding: 15px;
+    margin: 0 auto;
+  }
+  .form-signin .form-signin-heading,
+  .form-signin .checkbox {
+    margin-bottom: 10px;
+  }
+  .form-signin .checkbox {
+    font-weight: normal;
+  }
+  .form-signin .form-control {
+    position: relative;
+    font-size: 16px;
+    height: auto;
+    padding: 10px;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  .form-signin .form-control:focus {
+    z-index: 2;
+  }
+  .form-signin input[type="text"] {
+    margin-bottom: -1px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+  .form-signin input[type="password"] {
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
+  .account-wall {
+    margin-top: 20px;
+    padding: 40px 0px 20px 0px;
+    background-color: #f7f7f7;
+    -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  }
+
+  .login-title {
+    color: #555;
+    font-size: 18px;
+    font-weight: 400;
+    display: block;
+  }
+
+  .profile-img {
+    width: 96px;
+    height: 96px;
+    margin: 0 auto 10px;
+    display: block;
+    -moz-border-radius: 50%;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+  }
+
+  .need-help {
+    margin-top: 10px;
+  }
+
+  .new-account {
+    display: block;
+    margin-top: 10px;
+  }
+
+  .profile-img-card {
+    width: 96px;
+    height: 96px;
+    margin: 0 auto 10px;
+    display: block;
+    -moz-border-radius: 50%;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+  }
 </style>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,30 +1,27 @@
 <template>
   <div class="container">
     <div class="row justify-content-md-center">
-      <form class="form col-md-6 col-lg-4 text-justify">
-        <p v-if="errors.length">
-          <ul>
-            <li class="alert alert-danger"
-            v-for="error in errors"
-            :key="error">{{ error }}</li>
-          </ul>
-        </p>
-
-        <div class="form-group">
-          <label for="email">Cadastre seu email</label>
-          <input type="email" id="email" class="form-control"
-          placeholder="meu@email.com"
-          v-model="form.email" required>
+      <div class="col-sm-6 col-md-4 col-md-offset-4">
+        <h1 class="text-center login-title">Faça seu Cadastro</h1>
+        <div class="account-wall">
+          <img id="profile-img" class="profile-img-card"
+          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+          <p v-if="errors.length">
+            <ul class="alert alert-danger">
+              <li v-for="error in errors" :key="error">{{ error }}</li>
+            </ul>
+          </p>
+          <form class="form-signin">
+              <input type="email" id="email" class="form-control"
+                placeholder="Cadastre seu Email"
+                v-model="form.email" required>
+              <input type="password" id="password" class="form-control"
+              placeholder="Cadastre sua Senha"
+              v-model="form.password" required>
+              <button @click="register" class="btn btn-lg btn-primary btn-block">Cadastrar</button>
+          </form>
         </div>
-        <div class="form-group">
-          <label for="password">Cadastre sua Senha</label>
-          <input type="password" id="password" class="form-control"
-          placeholder="Cadastre sua Senha..." v-model="form.password" required>
-        </div>
-        <div class="float-right">
-          <button @click="register" class="btn btn-primary">Cadastrar</button>
-        </div>
-      </form>
+      </div>
     </div>
   </div>
 </template>
@@ -41,8 +38,6 @@ export default {
       form: {
         email: '',
         password: '',
-        emailConfirm: '',
-        passwordConfirm: '',
       },
     };
   },
@@ -52,6 +47,7 @@ export default {
       const form = this.form;
       firebase.auth().createUserWithEmailAndPassword(form.email, form.password)
         .then((user) => {
+          console.log(user);
           this.doRegister(user);
         })
         .catch((e) => {
@@ -76,10 +72,96 @@ export default {
         email: user.email,
         password: user.password,
       });
-      alert('Usuário Registrado');
-      // this.$router.push('/panel');
+      this.$router.push('/panel');
     },
   },
 };
 
 </script>
+
+<style scoped>
+.form-signin
+{
+    max-width: 330px;
+    padding: 15px;
+    margin: 0 auto;
+}
+.form-signin .form-signin-heading, .form-signin .checkbox
+{
+    margin-bottom: 10px;
+}
+.form-signin .checkbox
+{
+    font-weight: normal;
+}
+.form-signin .form-control
+{
+    position: relative;
+    font-size: 16px;
+    height: auto;
+    padding: 10px;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+.form-signin .form-control:focus
+{
+    z-index: 2;
+}
+.form-signin input[type="text"]
+{
+    margin-bottom: -1px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+}
+.form-signin input[type="password"]
+{
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+}
+.account-wall
+{
+    margin-top: 20px;
+    padding: 40px 0px 20px 0px;
+    background-color: #f7f7f7;
+    -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+}
+.login-title
+{
+    color: #555;
+    font-size: 18px;
+    font-weight: 400;
+    display: block;
+}
+.profile-img
+{
+    width: 96px;
+    height: 96px;
+    margin: 0 auto 10px;
+    display: block;
+    -moz-border-radius: 50%;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+}
+.need-help
+{
+    margin-top: 10px;
+}
+.new-account
+{
+    display: block;
+    margin-top: 10px;
+}
+.profile-img-card {
+    width: 96px;
+    height: 96px;
+    margin: 0 auto 10px;
+    display: block;
+    -moz-border-radius: 50%;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+}
+</style>
