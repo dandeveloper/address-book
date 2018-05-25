cfca30f6c8aec2d28859c855c053a025f0a16974
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-05-25 01:48:11 -0300

    Added vue-resource to the project

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import router from './router';
 import './firebase';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueResource);
 
 /* eslint-disable no-new */
 new Vue({
