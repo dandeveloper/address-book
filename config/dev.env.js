942816775c025b99fe89bc365a52b3f7f16b9993
Danilo de Carvalho Souza <danilotremere@gmail.com>
2018-07-01 10:57:40 -0300

    Change firebase values to .env file

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,14 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+require('dotenv').config()
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+  API_KEY: JSON.stringify(process.env.API_KEY),
+  AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+  DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+  PROJECT_ID: JSON.stringify(process.env.PROJECT_ID),
+  STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
+  MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID),
 })
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyC_F2fi2gcxY6kTSD_7FVQGVgNZOWUR0lw',
-  authDomain: 'address-book-d2077.firebaseapp.com',
-  databaseURL: 'https://address-book-d2077.firebaseio.com',
-  projectId: 'address-book-d2077',
-  storageBucket: 'address-book-d2077.appspot.com',
-  messagingSenderId: '152794032932',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
 };
 
 const app = firebase.initializeApp(config);
